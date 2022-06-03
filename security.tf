b69b6bcdec36cd4b12e8ab6a867714306c9e9d26
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-06-03 20:18:36 -0300

    Update security.tf

--- security.tf
+++ security.tf
@@ -49,3 +49,28 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+
+resource "aws_security_group" "allow_ssh_ec2" {
+  name        = "allow_ssh_ec2"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.VPC_with_Terraform_joaquin_aman.id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.2.192/26"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_ec2"
+  }
+}
